4605e348324bc2426e3d0d11a1ec6638154dfe84
Benji Dudu <benjamin.duhieu@epitech.eu>
2024-01-17 12:15:17 +0100

    Initial Commit

---
+++ PictarineTest/PictarineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictarineTestApp.swift
+//  PictarineTest
+//
+//  Created by Benjamin Duhieu on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PictarineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
